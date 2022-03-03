58043250548a3467ef231f767db46c85401c9489
Camille Sylla <camillesylla.web@gmail.com>
2022-03-03 16:10:14 +0100

    fixing header reqest

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const io = require("socket.io")(server, {
     cors : {
         origin: ["*"],
         methods: ["GET", "POST"],
-        credentials: true
+        handlePreflighRequest : (req, res) => {
+            res.writeHead(200, {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,POST",
+                "Access-Control-Allow-Headers": "my-custom-headers",
+                "Acces-Control-Allow-Credentials": true
+
+            })
+            res.end()
+        }
     }
 })
 
