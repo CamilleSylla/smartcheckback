f96f236a51258e458257e0ec2ca1e1df98183e8c
Camille Sylla <camillesylla.web@gmail.com>
2022-03-03 17:49:33 +0100

    url wasn't good

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://smartcheckfront.netlify.app",
+		origin: "*",
 		methods: [ "GET", "POST" ]
 	}
 });
